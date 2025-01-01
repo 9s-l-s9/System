49cb19d76bd5af52f9f255fca375c79086713368
s-l-s <schmidt.l.samuel@gmail.com>
2025-01-01 23:20:08 +0100

    Update colors

--- home/files/.config/stumpwm/quit.lisp
+++
@@ -1,8 +0,0 @@
-;; I would like to create a unique sleep and quit screen
-
-;;  Minimize everything including modeline.
-;;  Change wallpaper
-;; Ask for confirmation if one wants to quit.
-
-;; Maybe also add something similar for weather
-;; Maybe also as a welcome/splash screen?
--- home/files/.config/stumpwm/ui.lisp
+++ home/files/.config/stumpwm/ui.lisp
@@ -1,4 +1,16 @@
-;; Looks
+; Theme
+(setf *colors* '("#140A1D" ;; Black
+		 "#b52a5b" ;; Red
+		 "#ff4971" ;; Green
+		 "#8897f4" ;; Yellow
+		 "#bd93f9" ;; Blue
+		 "#e9729d" ;; Magenta
+		 "#f18fb0" ;; Cyan
+		 "#f1c4e0")) ;; White
+(update-color-map (current-screen))
+
+
+; Windows
 (setf *ignore-wm-inc-hints* t)
 (setf *message-window-gravity* :center)
 (setf *message-input-window-gravity* :center)
