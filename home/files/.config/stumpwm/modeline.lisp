8e85eec886ae6192c5d0095db9d9f7dc586fa088
s-l-s <schmidt.l.samuel@gmail.com>
2025-01-10 13:12:28 +0100

    Add padding to modeline

--- home/files/.config/stumpwm/modeline.lisp
+++ home/files/.config/stumpwm/modeline.lisp
@@ -4,7 +4,8 @@
   (setf *mode-line-background-color* base00)
   (setf *mode-line-border-color* base05)
   (setf *mode-line-foreground-color* base0B)
-
+  (setf *mode-line-pad-x* 5)
+  (setf *mode-line-pad-y* 5)
   ;; Mode line's contents
   (setf *group-format* "%t")
   (setf *screen-mode-line-format*
@@ -16,7 +17,7 @@
 
   ;; Refresh constantly
   (setf *mode-line-timeout* 10)
-
+  
   ;; Start mode line
   (mode-line))
 
