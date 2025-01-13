ae9e801655ce3d26eb26c147570c8a694ab41628
s-l-s <schmidt.l.samuel@gmail.com>
2025-01-13 22:59:34 +0100

    Add: compressed archive

--- archive/river/cpshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-shopt -s lastpipe
-slurp -d -w 4 -b '#00000044' -c '#ff0000' | read GEO
-if [ $? -eq 0 ]
-then
-	grim -g "${GEO}" -t png - | wl-copy
-fi
--- archive/river/poweroff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# poweroff prompt, intended to be launched by pressing the powerbutton. Requires
-# that logind does not handle the powerbutton.
-if wayprompt --title "Turn off device?" --button-ok "Yes" --button-cancel "No"
-then
-	/usr/bin/poweroff
-fi
