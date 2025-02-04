6ce3df52fa8a18f426776bbb200ccbba9e57abdf
s-l-s <schmidt.l.samuel@gmail.com>
2025-02-04 14:54:41 +0100

    Add date to todo command

--- home/files/.config/stumpwm/misc.lisp
+++ home/files/.config/stumpwm/misc.lisp
@@ -1,13 +1,17 @@
 (defcommand add-todo (todo-text) ((:string "Enter TODO: "))
-  "Add a new TODO to the org file"
-  (let* ((hostname (string-trim '(#\Newline) (run-shell-command "hostname" t)))
-         (todo-file (concatenate 'string "/home/samuel/Projects/WorkingMemory/wm-" hostname ".org")))
+  "Add a new TODO to the org file, including the current date."
+  (let* ((hostname (string-trim '(#\Newline)
+                                  (run-shell-command "hostname" t)))
+         (todo-file (concatenate 'string "/home/samuel/Projects/WorkingMemory/wm-" hostname ".org"))
+         (date-string (multiple-value-bind (sec min hour day month year)
+                           (get-decoded-time)
+                         (format nil "~4,'0d-~2,'0d-~2,'0d" year month day))))
     (with-open-file (stream todo-file
                             :direction :output
                             :if-exists :append
                             :if-does-not-exist :create)
       ;; Add a newline first to ensure separation from previous content
-      (format stream "~%* TODO ~A" todo-text))
+      (format stream "~%* TODO ~A ~A" date-string todo-text))
     (format t "Added TODO: ~A~%" todo-text)))
 
 
